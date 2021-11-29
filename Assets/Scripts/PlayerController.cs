456b7307c7689efe3044bcba1b8c5e4cdfb4e08b
sm42499 <89489372+sm42499@users.noreply.github.com>
2021-11-29 13:01:09 -0800

    some activities of moving the player are done. Two more activities to go.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+      
     }
 }
+
